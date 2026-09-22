0b969696316e7224da6ad5b8d495c16053a2cdaf
Lucas Fernandes | Full Stack Developer <akaakashiseijuro@gmail.com>
2026-09-22 13:08:10 -0300

    feat: add Fiapos app structure and admin

---
+++ app/admin/(protected)/agendamentos/page.tsx
@@ -0,0 +1,3 @@
+import {AdminShell} from '@/components/AdminShell';
+import {AdminAppointments} from '@/components/admin/AdminAppointments';
+export default function Page(){return <AdminShell><AdminAppointments/></AdminShell>}
