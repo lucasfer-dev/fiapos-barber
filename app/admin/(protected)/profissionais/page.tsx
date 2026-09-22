c81385acf6791c1b9673d1ca99d1d4eedf75f82a
Lucas Fernandes | Full Stack Developer <akaakashiseijuro@gmail.com>
2026-09-22 13:08:28 -0300

    feat: add Fiapos app structure and admin

---
+++ app/admin/(protected)/profissionais/page.tsx
@@ -0,0 +1,3 @@
+import {AdminShell} from '@/components/AdminShell';
+import {AdminEmployees} from '@/components/admin/AdminEmployees';
+export default function Page(){return <AdminShell><AdminEmployees/></AdminShell>}
